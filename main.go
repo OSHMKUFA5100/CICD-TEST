99c3bdd3762a6a16d476e2406a69339bc850964c
张育新 <zhangyuxin12301@163.com>
2025-11-25 14:48:57 +0800

    test1

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, CI/CD with Go!")
 	})
-	fmt.Println("Server listening on port 8080...")
+	fmt.Println("CI/CD-TEST Server listening on port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
